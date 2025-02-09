d0fc20a53d914c2e1142390cc7f8dc18463b057a
Chinmayee Hankare <riddhishak9@gmail.com>
2025-02-09 15:06:55 +0530

    Added user service interface

---
+++ backend/src/main/java/com/app/service/UserService.java
@@ -0,0 +1,18 @@
+package com.app.service;
+import com.app.dto.ApiResponse;
+import com.app.dto.AuthRequest;
+import com.app.dto.EditProfileDTO;
+import com.app.dto.UserRegister;
+import com.app.dto.UserRespDTO;
+
+import jakarta.validation.Valid;
+
+public interface UserService {
+
+	UserRespDTO signIn(@Valid AuthRequest dto);
+
+	ApiResponse register(UserRegister dto);
+
+	EditProfileDTO editProfile(EditProfileDTO dto, Integer id);
+
+}
